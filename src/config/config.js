34ae846a7f4e622c6ba259878c08b5b93822e6f2
paula <paulacruz.tg@gmail.com>
2025-08-27 13:40:35 -0500

    camio en env

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 3000,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
