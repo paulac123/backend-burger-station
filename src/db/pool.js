c11322f367b958daa1fe7984b38575ab8261b594
paula <paulacruz.tg@gmail.com>
2026-01-28 20:01:51 -0500

    Usar DATABASE_URL interna de Render para Postgres

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,16 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "dpg-d2n8rdf5r7bs73f8ktd0-a.oregon-postgres.render.com",
-  user: "burger_db_ic5f_user",
-  password: "3OKzq6Djk0SiTnCDWRUHGfcv7yC9DoCU",
-  database: "burger_db_ic5f",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   max: 20,
   idleTimeoutMillis: 30000,
-  ssl: {
-    rejectUnauthorized: false, // esto permite conexión con Render
-  },
 });
 
 module.exports = pool;
