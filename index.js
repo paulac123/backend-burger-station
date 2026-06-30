5c89c9d44365760d8d183854544ca095a935aba3
paulac <paulacruz.tg@gmail.com>
2026-06-30 09:39:53 -0500

    Fix: Orden de variables de entorno para evitar timeout de 34s

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require("./app");
 const { PORT } = require("./src/config/config");
+const app = require("./app");
 
 app.listen(PORT, () => {
   console.log(`servidor corriendo en el puerto${PORT}`);
